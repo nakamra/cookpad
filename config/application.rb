179af106b24d12798be0400e10cb96458ca28609
taka_nakamra <Home@tnmbp.local>
2017-02-22 16:31:53 +0900

    Add config.generators in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,6 @@
 require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,11 +11,10 @@ module Cookpad
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.template_engine :haml
+    end
   end
-end
-
-config.generators do |g|
-  g.javascripts false
-  g.stylesheets false
-  g.template_engine :haml
 end
