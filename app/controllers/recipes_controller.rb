cab25b72f639769504687bcd2b1a4659ea7148dd
taka_nakamra <Home@tnmbp.local>
2017-03-04 19:01:04 +0900

    Add post action in recipes_controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,13 @@
 class RecipesController < ApplicationController
-  before_action :set_default_value, only: [:new, :index]
+  before_action :set_default_value, only: [:new, :index, :post]
 
   def new
   end
 
   def index
+  end
+
+  def post
   end
 
   private
