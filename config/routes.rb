5994231fca81ae43952ab2bf548c2308417bcf81
taka_nakamra <Home@tnmbp.local>
2017-03-04 19:00:21 +0900

    Add post routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "recipes#index"
-  resources :recipes, only: [:index, :new]
+  resources :recipes, only: [:index, :new] do
+    collection do
+      get 'post'
+    end
+  end
 
 end
