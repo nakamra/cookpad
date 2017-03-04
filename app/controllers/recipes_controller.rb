5e9745e7b7574329367e9627aba5861ffcb5d9a6
taka_nakamra <Home@tnmbp.local>
2017-03-04 13:22:57 +0900

    set before_action default__value

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,14 @@
 class RecipesController < ApplicationController
+  before_action :set_default_value, only: [:new, :index]
+
+  def new
+  end
 
   def index
+  end
+
+  private
+  def set_default_value
     @recipe = Recipe.new
   end
 end
