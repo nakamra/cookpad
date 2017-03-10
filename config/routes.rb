62856daa8818f9883fefa4da74340e12a476b8f3
taka_nakamra <Home@tnmbp.local>
2017-03-10 20:40:58 +0900

    Add recipes show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "recipes#index"
-  resources :recipes, only: [:index, :new] do
+  resources :recipes, only: [:index, :new, :show] do
     collection do
       get 'post'
     end
