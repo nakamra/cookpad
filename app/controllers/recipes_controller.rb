db8347a18f18ba2df63a3a4187ea388f77341e21
taka_nakamra <Home@tnmbp.local>
2017-05-08 16:17:41 +0900

    Add Create action and params method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :set_default_value
+  before_action :set_default_value, only: [:new, :index, :show, :edit]
 
   def new
   end
@@ -7,13 +7,27 @@ class RecipesController < ApplicationController
   def index
   end
 
-  def post
+  def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def show
   end
 
+  def create
+    @recipe = current_user.recipes.new(recipe_name_params)
+    if @recipe.save
+      redirect_to edit_recipe_path(@recipe)
+    else
+      render "new"
+    end
+  end
+
   private
+  def recipe_name_params
+    params.require(:recipe).permit(:id, :name)
+  end
+
   def set_default_value
     @recipe = Recipe.new
   end
