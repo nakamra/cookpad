e4dd48862b44ca27f540e5fb39c664b016e495a3
taka_nakamra <Home@tnmbp.local>
2017-05-19 21:11:01 +0900

    Create IngredientsController create action

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,14 @@
 class IngredientsController < ApplicationController
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = @recipe.ingredients.new(ingredient_params)
+      if @ingredient.save
+        redirect_to edit_recipe_path(@recipe)
+      end
+  end
+
+  private
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :quantity)
+  end
 end
