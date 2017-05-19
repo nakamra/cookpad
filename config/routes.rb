5bef40ec9707ba671778c9ce24125f72b4cdf71a
taka_nakamra <Home@tnmbp.local>
2017-05-19 21:08:14 +0900

    Add ingredients routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "recipes#index"
   resources :recipes, only: [:index, :new, :edit, :show, :create] do
+    resources :ingredients, only: [:create]
     collection do
       get 'search'
     end
