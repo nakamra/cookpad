8ebe9eb019859d36636e435bbb83516304d43237
taka_nakamra <Home@tnmbp.local>
2017-05-26 14:28:54 +0900

    Add update routing in routes/.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "recipes#index"
-  resources :recipes, only: [:index, :new, :edit, :show, :create] do
+  resources :recipes, only: [:index, :new, :edit, :show, :create, :update] do
     resources :ingredients, only: [:create]
     collection do
       get 'search'
