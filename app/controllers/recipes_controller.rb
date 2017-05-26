ad1f5bf22ba0f41c2360404b157aee1faf899328
taka_nakamra <Home@tnmbp.local>
2017-05-26 14:33:26 +0900

    Create recipe_ingredient_params define, edit action, update action for ingredient input in recipes_controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,17 +1,10 @@
 class RecipesController < ApplicationController
   before_action :set_default_value, only: [:new, :index, :show, :edit]
 
-  def new
-  end
-
   def index
   end
 
-  def edit
-    @recipe = Recipe.find(params[:id])
-  end
-
-  def show
+  def new
   end
 
   def create
@@ -23,9 +16,34 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+    @recipe.ingredients.build
+    @ingredient = Ingredient.new()
+    @ingredients = @recipe.ingredients
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_ingredient_params)
+      redirect_to edit_recipe_path(@recipe)
+    else
+      render "edit"
+    end
+  end
+
+  def show
+  end
+
+
+
   private
   def recipe_name_params
     params.require(:recipe).permit(:id, :name)
+  end
+
+  def recipe_ingredient_params
+    params.require(:recipe).permit(ingredients_attributes: [:name, :quantity, :_destroy, :id])
   end
 
   def set_default_value
