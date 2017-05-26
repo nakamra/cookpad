7ba3509c1f654fff16706f2623c484c39f34d925
taka_nakamra <Home@tnmbp.local>
2017-05-26 14:30:16 +0900

    Add accepts_nested_attributes_for for ingredients in recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,5 @@ class Recipe < ApplicationRecord
   validates :name, uniqueness: { scope: :user_id }
   belongs_to :user
   has_many :ingredients
+  accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: :all_blank
 end
